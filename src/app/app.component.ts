cbc070270fcff9954f616d563290cc86a4c480c2
Muhammad Amin <muhammad.amin.8102@gmail.com>
2025-01-12 03:05:12 +0530

    Final commit with sorting,drawer,footer,badges on rewards images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from "./header/header.component";
 import { ContentComponent } from "./content/content.component";
 import { FooterComponent } from "./footer/footer.component";
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ContentComponent, FooterComponent, FormsModule],
+  imports: [RouterOutlet, HeaderComponent, ContentComponent, FooterComponent, FormsModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
